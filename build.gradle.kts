ed512d00fb2ada83bc3b3d1dd616fdfd42f81d8f
vdisk <vdisk@foxmail.com>
2023-11-08 09:37:51 +0800

    fix: gradle build

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2022.1.3")
+    version.set("2023.2.4")
     type.set("IC") // Target IDE Platform
 
     plugins.set(listOf("java"))
@@ -31,7 +31,7 @@ tasks {
     }
 
     patchPluginXml {
-        sinceBuild.set("222")
+        sinceBuild.set("232")
         untilBuild.set("232.*")
     }
 
